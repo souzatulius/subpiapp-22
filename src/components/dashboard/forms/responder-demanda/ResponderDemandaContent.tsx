5df2f47bb707d4021c689a1af721e3c05b896b13
subpinheiros <tmtsouza@smsub.prefeitura.sp.gov.br>
2025-03-27 20:04:36 -0300

    Update ResponderDemandaContent.tsx

--- src/components/dashboard/forms/responder-demanda/ResponderDemandaContent.tsx
+++ src/components/dashboard/forms/responder-demanda/ResponderDemandaContent.tsx
@@ -1,4 +1,3 @@
-import { Badge } from '@/components/ui/badge';
 import React, { useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
