19741d357331870e713416f603de9db287d972f6
subpinheiros <tmtsouza@smsub.prefeitura.sp.gov.br>
2025-03-27 19:13:07 -0300

    Create ServicoSection.tsx

---
+++ src/components/dashboard/forms/responder-demanda/components/sections/ServicoSection.tsx
@@ -0,0 +1,13 @@
+// src/components/dashboard/forms/responder-demanda/components/sections/ServicoSection.tsx
+import React from 'react';
+
+const ServicoSection: React.FC = () => {
+  return (
+    <div>
+      <h3>Seção Serviço</h3>
+      {/* Você pode implementar o componente corretamente depois */}
+    </div>
+  );
+};
+
+export default ServicoSection;
