2364c24247687ae618a1548a8993148c301800da
subpinheiros <tmtsouza@smsub.prefeitura.sp.gov.br>
2025-03-29 22:35:53 -0300

    Create formSchema.ts

---
+++ src/types/formSchema.ts
@@ -0,0 +1,16 @@
+import { CardColor, CardType, DataSourceKey } from './dashboard';
+
+export interface FormSchema {
+  title: string;
+  type?: CardType;
+  path?: string;
+  color: CardColor;
+  iconId?: string;
+  width?: '25' | '50' | '75' | '100';
+  height?: '1' | '2';
+  dataSourceKey?: DataSourceKey;
+  displayMobile?: boolean;
+  mobileOrder?: number;
+  allowedDepartments?: string[];
+  allowedRoles?: string[];
+}
